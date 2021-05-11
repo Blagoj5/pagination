0fde017b4a256ccabea924b3e53e81477264f9b0
Blagoj <blagoj.petrov5@gmail.com>
2021-05-12 00:40:45 +0200

    Clean example code

--- example/components/elements/HeadingMedium.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-interface HeadingBigProps {
-  children: string;
-}
-
-export const HeadingBig: React.FC<HeadingBigProps> = ({ children }) => {
-  return <h2 className="text-xl">{children}</h2>;
-};
--- example/components/elements/HeadingSmall.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-interface HeadingSmallProps {
-  children: string;
-}
-
-export const HeadingSmall: React.FC<HeadingSmallProps> = ({ children }) => {
-  return <h2 className="text-lg font-bold mb-2">{children}</h2>;
-};
